83e096d08b7fc15a8cdae0da75c5ae3b76982a7d
hamiltonmbsilva <hamiltonmb.silva@gmail.com>
2021-01-19 11:02:47 -0300

    Criando o projeto

---
+++ Alura.LeilaoOnline.ConsoleAPP/Program.cs
@@ -0,0 +1,25 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+
+namespace Alura.LeilaoOnline.ConsoleAPP
+{
+    class Program
+    {
+        static void Main()
+        {
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var Maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(Maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(Maria, 990);
+            leilao.RecebeLance(Maria, 999);
+
+            leilao.TerminaPregao();
+
+            Console.WriteLine(leilao.Ganhador.Valor);
+        }
+    }
+}
