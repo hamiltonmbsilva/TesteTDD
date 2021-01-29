36f2e75cfed63f2fac090d5a4c919002bfc94b84
hamiltonmbsilva <hamiltonmb.silva@gmail.com>
2021-01-29 16:43:03 -0300

    Aprendendo sobre anotação Theory cria varios teste para amesma entrada

---
+++ Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,55 @@
+﻿using Alura.LeilaoOnline.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double [] ofertas)
+        {
+            //Arranje - Cénario
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            
+
+            foreach(var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+            
+
+            //Act - método sob teste
+            leilao.TerminaPregao();
+
+            //Assert            
+            var valorObitido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObitido);
+
+        }
+
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLance()
+        {            
+                //Arranje - Cénario
+                var leilao = new Leilao("Van Gogh");  
+
+                //Act - método sob teste
+                leilao.TerminaPregao();
+
+                //Assert
+                var valorEsperado = 0;
+                var valorObitido = leilao.Ganhador.Valor;
+
+                Assert.Equal(valorEsperado, valorObitido);            
+        }
+
+        
+    }
+}
