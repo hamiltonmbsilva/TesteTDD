1ee649575908b895946b2b58099f1ea981d5320d
hamiltonmbsilva <hamiltonmb.silva@gmail.com>
2021-02-02 10:26:03 -0300

    Criando novos testes

--- Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
+++ Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -6,6 +6,26 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoRecebeOferta
     {
+        [Fact]
+        public void NaoAceitaProximoLanceDadoMesmoClienteRealizouUltimoLance()
+        {
+            //Arranje - Cénario
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.IniciaPregao();
+            leilao.RecebeLance(fulano, 800);           
+
+
+            //Act - método sob teste
+            leilao.RecebeLance(fulano, 1000);
+
+            //Assert 
+            var qtdEsperada = 1;
+            var qtdObitido = leilao.Lances.Count();
+            Assert.Equal(qtdEsperada, qtdObitido);
+        }
+
         [Theory]
         [InlineData(4, new double[] { 1000, 1200, 1400, 1300})]
         [InlineData(2, new double[] { 800, 900})]
